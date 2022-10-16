822b448ffdec1019e0b7637eb25a7667a9cbf1f4
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-10-16 09:24:23 +0530

    Update StringPermute.java

--- Algorithms/Backtracking/StringPermute/StringPermute.java
+++ Algorithms/Backtracking/StringPermute/StringPermute.java
@@ -16,34 +16,30 @@ class StringPermute {
   return true;
 }
 
-	private void permute(String str, int l, int r) {
-        if (l == r)
-    System.out.print(str + " ");
-    else{
-
-    for (int i = l; i <= r; i++){
-
-        if (isSafe(str, l, i, r)) {
-
-       str = swap(str, l, i);
-       permute(str, l + 1, r);
-       str = swap(str, l, i);
-        }
-    }
-}
+       private void permute(String str, int l, int r) {
+       if (l == r) System.out.print(str + " ");
+       else{
+	       for (int i = l; i <= r; i++){
+		       if (isSafe(str, l, i, r)) {
+			       str = swap(str, l, i);
+			       permute(str, l + 1, r);
+			       str = swap(str, l, i);
+        		}
+    		}
+	}
 }
 	public String swap(String a, int i, int j) {
-        char temp;
-        char[] charArray = a.toCharArray();
-        temp = charArray[i];
-        charArray[i] = charArray[j];
-        charArray[j] = temp;
-        return String.valueOf(charArray);
-    }
+		char temp;
+		char[] charArray = a.toCharArray();
+		temp = charArray[i];
+		charArray[i] = charArray[j];
+		charArray[j] = temp;
+		return String.valueOf(charArray);
+    	}
 
 	public static void main(String args[]) {
 
-		String str = "ABC";
+	String str = "ABC";
         int n = str.length();
         StringPermute permutation = new StringPermute();
 
