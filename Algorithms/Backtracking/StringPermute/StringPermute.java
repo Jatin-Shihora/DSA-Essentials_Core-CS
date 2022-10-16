9b9c2096f5b150dd4917b4836db3abf6c6e53cbb
Dmitriy S <contact@dhotspot.xyz>
2022-10-15 23:13:20 -0400

    Class name fix + enhancement. Issue #24
    I have fixed the class name as said in the issue, as well as enhanced the code making it look better and organized

--- Algorithms/Backtracking/StringPermute/StringPermute.java
+++ Algorithms/Backtracking/StringPermute/StringPermute.java
@@ -1,42 +1,38 @@
-public class Permutations {
-	
+class StringPermute {
+
 	public boolean isSafe(String str, int l, int i, int r) {
-		if (l != 0 && str.charAt(l - 1) == 'A'
+		if (l != 0
+                && str.charAt(l - 1) == 'A'
 	            && str.charAt(i) == 'B')
 	            return false;
-		if (r == l + 1 && str.charAt(i) == 'A'
+		if (r == l + 1
+                && str.charAt(i) == 'A'
                 && str.charAt(l) == 'B'
-                || r == l + 1 && l == i 
+                || r == l + 1 && l == i
                 && str.charAt(r) == 'B'
                 && str.charAt(l) == 'A')
       return false;
 
   return true;
-	}
-	
-	private void permute(String str, 
-            int l, int r)
-{
+}
 
-if (l == r)
-System.out.print(str + " ");
-else
-{
+	private void permute(String str, int l, int r) {
+        if (l == r)
+    System.out.print(str + " ");
+    else{
 
-for (int i = l; i <= r; i++)
-{
-   
-   if (isSafe(str, l, i, r)) 
-   {
+    for (int i = l; i <= r; i++){
+
+        if (isSafe(str, l, i, r)) {
+
        str = swap(str, l, i);
        permute(str, l + 1, r);
        str = swap(str, l, i);
-   }
+        }
+    }
 }
 }
-}
-	public String swap(String a, int i, int j)
-    {
+	public String swap(String a, int i, int j) {
         char temp;
         char[] charArray = a.toCharArray();
         temp = charArray[i];
@@ -45,13 +41,13 @@ for (int i = l; i <= r; i++)
         return String.valueOf(charArray);
     }
 
-	public static void main(String[] args) {
-		
+	public static void main(String args[]) {
+
 		String str = "ABC";
         int n = str.length();
-        Permutations permutation = new Permutations();
- 
+        StringPermute permutation = new StringPermute();
+
         // Function call
         permutation.permute(str, 0, n - 1);
-
-	}
+    }
+}
