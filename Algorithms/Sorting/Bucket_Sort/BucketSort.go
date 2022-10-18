663430266686d2b7684046e17383e94d2313fb41
JAWAD KHAN <jawadkhan.tech@gmail.com>
2022-10-18 21:09:26 +0500

    Update BucketSort.go

--- Algorithms/Sorting/Bucket_Sort/BucketSort.go
+++ Algorithms/Sorting/Bucket_Sort/BucketSort.go
@@ -1,3 +1,6 @@
+/*
+Bucket sort, or bin sort, is a sorting algorithm that works by distributing the elements of an array into a number of buckets. Each bucket is then sorted individually, either using a different sorting algorithm, or by recursively applying the bucket sorting algorithm.
+*/
 
 package main
 
@@ -7,7 +10,7 @@ import (
 	"strconv"
 )
 
-
+// Method for Insertion Sort containig array in Float
 func insertionSort(array []float64) {
 	for i := 0; i < len(array); i++ {
 		temp := array[i]
@@ -19,6 +22,7 @@ func insertionSort(array []float64) {
 	}
 }
 
+// Method for bucketSort
 func bucketSort(array []float64, bucketSize int) []float64 {
 	var max, min float64
 	for _, n := range array {
@@ -34,7 +38,7 @@ func bucketSort(array []float64, bucketSize int) []float64 {
 	for i := 0; i < nBuckets; i++ {
 		buckets[i] = make([]float64, 0)
 	}
-
+	// for loop to iterate the range of an array
 	for _, n := range array {
 		idx := int(n-min) / bucketSize
 		buckets[idx] = append(buckets[idx], n)
@@ -47,12 +51,15 @@ func bucketSort(array []float64, bucketSize int) []float64 {
 			sorted = append(sorted, bucket...)
 		}
 	}
-
+	// returns sorted array
 	return sorted
 }
 
+// main method from where program starts execution
 func main() {
 
+	
+	// static array
 	array :=[]float64{4, 1, 1, 3, 2, 2, 7, -6, 0}
 	for _, arg := range os.Args[1:] {
 		if n, err := strconv.ParseFloat(arg, 64); err == nil {
@@ -60,6 +67,7 @@ func main() {
 		}
 	}
 	fmt.Printf("Before Bucket Sort %v\n", array)
+	// calling method bucket sort
 	array = bucketSort(array, 5)
 	fmt.Printf("After Bucket Sort %v\n", array)
 }
